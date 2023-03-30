57ae735860280746234c2b06462a9553cfb8a5ae
Leonardo Petruncko <62786185+leonardoPetruncko@users.noreply.github.com>
2023-03-30 05:16:33 -0300

    Rodape css a link

--- src/components/Rodape/index.js
+++ src/components/Rodape/index.js
@@ -2,8 +2,8 @@ import styles from "./Rodape.module.css"
 
 export default function Rodape() {
     return (
-        <footer>
-            <h2 className={styles.rodape}>Desenvolvido por <a className={styles.name}> Leonardo Petruncko </a></h2>
+        <footer className={styles.rodape}>
+            <h2 className={styles.texto}>Desenvolvido por <a>Leonardo Petruncko</a></h2>
         </footer>
     )
 }
