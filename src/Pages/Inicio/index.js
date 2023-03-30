f136b605efce491f084c3097b1556f44f31542da
Leonardo Petruncko <62786185+leonardoPetruncko@users.noreply.github.com>
2023-03-30 19:19:31 -0300

    Component Titulo

--- src/Pages/Inicio/index.js
+++ src/Pages/Inicio/index.js
@@ -1,6 +1,7 @@
 import Banner from "components/Banner";
 import Cabecalho from "components/Cabecalho";
 import Rodape from "components/Rodape";
+import Titulo from "components/Titulo";
 
 
 export default function Inicio() {
@@ -8,6 +9,9 @@ export default function Inicio() {
         <>
             <Cabecalho />
             <Banner imagem="home" />
+            <Titulo>
+                <h1>Filmes e videos dale</h1>
+            </Titulo>
             <Rodape />
         </>
     )
